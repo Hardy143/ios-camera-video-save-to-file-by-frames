52c5896759b28eeee418f140dea299f5eb2c4ffe
ZhaonanLi <zhaonanproject@gmail.com>
2016-08-13 20:29:56 -0700

    Initial Commit

---
+++ ios-camera-video-save-to-file-by-frames/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-camera-video-save-to-file-by-frames
+//
+//  Created by Zhaonan Li on 8/13/16.
+//  Copyright © 2016 Zhaonan Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
